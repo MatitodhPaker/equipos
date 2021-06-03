2ebdfe866182d200035dfe9f3033bb095cb7d69e
MatitodhPaker <hueco35.perez@gmail.com>
2021-06-02 19:47:05 -0500

    se corrije el archivo de ingresarequipo.php

--- ingresarequipo.php
+++ ingresarequipo.php
@@ -22,57 +22,20 @@
       <div class="row">
         <div class="col">
         <form method="post" onsubmit="return calcular()" id="frmcmoneda">
-                <div class="form-group">
-                  <label for="monto">Monto a gastar</label>
-                  <div class="row">
-                  <div class="col-sm-1">$</div>
-                  <div class="col-sm-8"><input type="number" id="monto" class="form-control" placeholder="500" required pattern="[0-9]+" min="1" max="800000"></div>
-                  <div class="col">mxn</div>
-                  </div>
-                  <small id="helpId" class="text-muted">ingrese la cantidad deseada a cambiar</small>
-                </div>
-                <div class="form-group">
-                  <label for="radios">Moneda a comprar:</label>
-                  <div class="row">
-                    <div class="col">
-                      <label for="bitcoin">Bitcoin</label>
-                      <input type="radio" name="moneda" id="bitcoin" value="bitcoin">
-                      <br>
-                      <label for="mana">Mana</label>
-                      <input type="radio" name="moneda" id="mana" value="mana">
-                      <br>
-                      <label for="bat">Bat</label>
-                      <input type="radio" name="moneda" id="bat" value="bat">
-                    </div>
-                    <div class="col">
-                      <label for="etherium">Etherium</label>
-                      <input type="radio" name="moneda" id="etherium" value="etherium">
-                      <br>
-                      <label for="xrp">Xrp</label>
-                      <input type="radio" name="moneda" id="xrp" value="xrp">
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="row">
-                    <div class="col">
-                    <button class="btn btn-info btn-block">cobrar</button>
-                    </div>
-                    <div class="col">
-                      <span class="btn btn-danger btn-block" onclick="limpiar()">limpiar</span>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col">
-                      <label>Tus monedas son</label>
-                      <div class="row">
-                        <div class="col-sm-8"><input type="number" id="total" class="form-control" disabled></div>
-                        <div class="col" id="cmoneda"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </form>
+        <div class="form-group">
+            <div class="row">
+              <div class="col">
+                <div id="idUsuario" hidden></div>
+                <label for="nombreU">Nombre</label>
+                <input type="text" name="nombreU" id="nombreU" class="form-control" required>
+                <label for="modeloU">Modelo</label>
+                <input type="text" name="modeloU" id="modeloU" class="form-control">
+                <label for="numeroserieU">Numero de serie</label>
+                <input type="text" name="numeroserieU" id="numeroserieU" class="form-control">
+              </div>
+            </div>
+          </div>
+        </form>
         </div>
       </div>
     </div>
